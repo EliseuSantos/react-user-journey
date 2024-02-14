bacf888efc1a348ff2fc77715cece4df868a7f41
Eliseu dos Santos da Silva <s.eliseu.santos@gmail.com>
2024-02-14 18:55:25 -0300

    fix: fix ci/cd

--- release.config.js
+++ release.config.js
@@ -1,11 +1,29 @@
 module.exports = {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-    '@semantic-release/git',
-  ],
+    branches: ['main'],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogTitle": "# Changelog"
+            }
+        ],
+        [
+            "@semantic-release/npm",
+            {
+                "pkgRoot": "dist"
+            }
+        ],
+        '@semantic-release/github',
+        [
+            "@semantic-release/git",
+            {
+                "message": "chore(release): ${nextRelease.version} [skip ci]",
+                "assets": [
+                    "CHANGELOG.md"
+                ]
+            }
+        ]
+    ],
 };
